85d99c7eae146101913a8f981f59dd44924597a6
d-roman-halliday <david@roman-halliday.com>
2022-07-25 20:39:57 +0100

    Adding changes for a Dynamic Reporting model (using object)

--- dmdma/data/reporting/testing_schema.sql
+++ dmdma/data/reporting/testing_schema.sql
@@ -1,5 +1,7 @@
 DROP TABLE IF EXISTS testing;
 
+DROP TABLE IF EXISTS testing_2;
+
 CREATE TABLE testing (
   id       INTEGER PRIMARY KEY AUTOINCREMENT,
   value_1  TEXT UNIQUE NOT NULL,
@@ -13,4 +15,23 @@ VALUES(1, 'v1', 'g1',   1),
       (2, 'v2', 'g1',  10),
       (3, 'v3', 'g2',  20),
       (4, 'v4', NULL,  50)
+;
+
+CREATE TABLE testing_2 (
+  id       INTEGER PRIMARY KEY AUTOINCREMENT,
+  value_1  TEXT UNIQUE NOT NULL,
+  value_2  TEXT,
+  metric_1 NUMERIC
+);
+
+INSERT
+  INTO testing_2
+      (value_1,
+       value_2,
+       metric_1
+      )
+VALUES('vA', NULL,  10),
+      ('vB', NULL,  10),
+      ('vC', NULL,  10),
+      ('vD', NULL,  10)
 ;
--- dmdma/reporting/item.py
+++ dmdma/reporting/item.py
@@ -1,24 +1,47 @@
 '''
-Created on 16 Jul 2022
-
 @author: davidroman-halliday
 '''
-class item:
+import uuid
+
+from dmdma.reporting import beautify_html_table
+
+class reportItem:
     """A common reporting set of attributes to iterate over for multiple data sets to be returned in a page"""
     def __init__(self,
-                title : str = None,
-                DataFrame = None,
-                DataFrameID : str = None,
-                DataFrameTitlesCentered : bool = True,
-                text : str = None,
-                html : str = None
+                title,
+                dataFrame = None,
+                introText : str = None,
+                introTextIsHTML : bool = False,
+                outroText : str = None,
+                outroTextIsHTML : bool = False,
+                htmlTable : str = None,
+                htmlTableID : str = None,
+                htmlTableTitlesCentered : bool = True,
+                htmlTableCsvDownload : bool = True
                 ):
+
+        if htmlTableID is None:
+            htmlTableID = uuid.uuid4()
+
         self.title = title
-        self.DataFrame = DataFrame
-        self.DataFrameID = DataFrameID
-        self.DataFrameTitlesCentered = DataFrameTitlesCentered
-        self.text = text
-        self.html = html
+        self.dataFrame = dataFrame
+
+        self.introText = introText
+        self.introTextIsHTML = introTextIsHTML
+        self.outroText = outroText
+        self.outroTextIsHTML = outroTextIsHTML
+
+        self.htmlTable = htmlTable
+        self.htmlTableID = htmlTableID
+        self.htmlTableTitlesCentered = htmlTableTitlesCentered
+        self.htmlTableCsvDownload = htmlTableCsvDownload
+
+    def convertToBeautifulHTML(self):
+        self.htmlTable = beautify_html_table(
+            html_table = self.dataFrame.to_html(index = False),
+            table_id = self.htmlTableID,
+            center_headings = self.htmlTableTitlesCentered
+            )
 
 if __name__ == '__main__':
     pass
