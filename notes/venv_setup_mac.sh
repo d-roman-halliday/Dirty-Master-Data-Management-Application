c181c57109b128408726426d5c7adf2eebee2f4a
David Roman-Halliday <david.halliday@gmail.com>
2022-11-04 18:34:18 +0000

    Changes

--- notes/venv_setup_mac.sh
+++ notes/venv_setup_mac.sh
@@ -1,12 +1,34 @@
+################################################################################
+# Notes on seting up appropriate venv on mac for dev
+################################################################################
 
+# Understand where we are
+which python
+python --version
+pip list
 
-python3.9 -m venv venv
+# Manage/change base environments as required (pyenv etc)
+# Won't try to document here
+
+# Make sure we are in project working directory
+
+# Venv (seems better on Mac with Eclipse)
+python<interpreter version preffered> -m venv venv
+
+# Virtualenv (alternative to venv) - as an option, but didn't work so well for me
+pip install virtualenv
+virtualenv venv
+
+# Start Venv (whichever method from above)
 source venv/bin/activate
+
+# Understand where we are (again)
+which python
+python --version
+pip list
+
+# Install packages & dependencies
 pip install --upgrade pip
 
-pip install Flask
-pip install bootstrap_flask
-
-pip install pandas
-
-pip install beautifulsoup4
+# pip install -e .
+pip install -e Dirty-Master-Data-Management-Application
